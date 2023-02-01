45b76c470e4c680f4de942e1eacb1eb918e128c0
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2023-02-01 13:30:12 +0530

    working on RegisterSellerActivity.java...

--- app/src/main/java/com/example/flashcart/RegisterSellerActivity.java
+++ app/src/main/java/com/example/flashcart/RegisterSellerActivity.java
@@ -1,24 +1,68 @@
 package com.example.flashcart;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.loader.app.LoaderManager;
 
+import  android.Manifest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterSellerActivity extends AppCompatActivity {
+import java.net.URI;
+import java.util.List;
+import java.util.Locale;
+
+public class RegisterSellerActivity extends AppCompatActivity implements LocationListener {
 
 
     ImageButton sellerbackbtn, sellermylocation;
     ImageView sellerprofile;
 
-    EditText email,pass,name,phone,country,state,city;
+    EditText email,pass,name,phone,countryET,stateET,cityET,addressET;
     Button sellerregisterbtn;
+
+    //creating variable for location
+
+    private static final int LOCATION_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 300;
+    private static final int IMAGE_PICK_GALLERY_CODE = 400;
+    private static final int IMAGE_PICK_CAMERA_CODE = 500;
+
+    //creating permission array
+
+    private String[] locationPermissions;
+    private String[] cameraPermission;
+    private String[] storagePermission;
+    private LocationManager locationManager;
+    private double longitude,latilude;
+    private Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +79,18 @@ public class RegisterSellerActivity extends AppCompatActivity {
         name = findViewById(R.id.sellerName);
         pass = findViewById(R.id.sellerpassword);
         phone = findViewById(R.id.sellerPhone);
-        country = findViewById(R.id.sellercountry);
-        state = findViewById(R.id.sellerstate);
-        city = findViewById(R.id.sellercity);
+        countryET = findViewById(R.id.sellercountry);
+        stateET = findViewById(R.id.sellerstate);
+        cityET = findViewById(R.id.sellercity);
+        addressET = findViewById(R.id.selleraddress);
         sellerregisterbtn = findViewById(R.id.sellerregisterbtn);
 
 
+       //initializing perssmison location
+
+        locationPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
 
 
@@ -51,5 +101,209 @@ public class RegisterSellerActivity extends AppCompatActivity {
             }
         });
 
+        sellermylocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // fetch the location
+                if(checkLocationPermission()){
+                    detectLocation();
+                }else{
+                    requestLocationPermission();
+                }
+
+            }
+        });
+
+        sellerprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pick image
+                showImagePickDialoge();
+            }
+        });
+
+
+
+
+
+
+
     }
+
+    private void showImagePickDialoge(){
+        String[] option = {"Camera","Gallery"};
+
+        //show dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick Your Image")
+                .setItems(option, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if(which == 0){
+                          // then camera clicked
+                        }else{
+                            //otherwise gallery clicked
+                        }
+
+                    }
+                })
+                .show();
+    }
+
+    private void clickedFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickFromCamera(){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_Image Title");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp_Image Descritption");
+
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(intent, IMAGE_PICK_CAMERA_CODE);
+
+
+    }
+
+    private void checkStoragePermission(){
+        //remain from here
+    }
+
+    private void detectLocation() {
+
+        Toast.makeText(this,"Please wait while location is being fetched .....",Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+
+    }
+
+    private boolean checkLocationPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == (PackageManager.PERMISSION_GRANTED);
+
+        return result;
+    }
+
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(this,locationPermissions,LOCATION_REQUEST_CODE);
+    }
+
+
+
+    // all methods of "implement location listener"
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latilude = location.getLatitude();
+        longitude = location.getLongitude();
+
+        findAddress();
+
+    }
+
+    private void findAddress(){
+
+        //here we will find address country state and city
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+
+        try {
+
+            addresses = geocoder.getFromLocation(latilude,longitude,1);
+            String address = addresses.get(0).getAddressLine(0);
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+
+            //now we will set the address to editbox
+
+           countryET.setText(country);
+           stateET.setText(state);
+           cityET.setText(city);
+           addressET.setText(address);
+
+
+
+
+        }catch (Exception e){
+           Toast.makeText(this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+        Toast.makeText(this,"Please enable Loaction service", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+
+
+    // "Location Listener up to these"
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case LOCATION_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    boolean loacationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if(loacationAccepted){
+
+                        //permission given
+
+                    }else{
+
+                        //permission not given
+                        Toast.makeText(this,"Loaction Permission is nessasory before procedding",Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
 }
