5de3ebc35e6741a1bbc247fbbba7b5adee1b3d91
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2023-02-07 13:01:38 +0530

    worked on login signup page........

--- app/src/main/java/com/example/flashcart/MainUserActivity.java
+++ app/src/main/java/com/example/flashcart/MainUserActivity.java
@@ -1,14 +1,135 @@
 package com.example.flashcart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.flashcart.databinding.ActivityMainBinding;
+import com.example.flashcart.databinding.ActivityMainUserBinding;
+import com.example.flashcart.fragments.ProfileFragment;
+import com.example.flashcart.fragments.UserHomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
 
 public class MainUserActivity extends AppCompatActivity {
+
+    private TextView nameTV;
+    private ImageButton logoutbtn;
+
+    private FirebaseAuth firebaseAuth;
+
+    ActivityMainUserBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_user);
+
+        binding = ActivityMainUserBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        //bydefault fragment would be home page of use
+
+        replaceFragment(new UserHomeFragment());
+
+        binding.bottomnavigationBar.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.Home:
+                    replaceFragment(new UserHomeFragment());
+                    break;
+                case R.id.Category:
+                    break;
+                case R.id.Notification:
+                    break;
+                case R.id.Cart:
+                    break;
+                case R.id.Account:
+                    replaceFragment(new ProfileFragment());
+                    break;
+
+            }
+
+
+            return true;
+        });
+
+        //currently we will not use it for now to diaplay name to home page
+
+//        nameTV = findViewById(R.id.nametv);
+//        logoutbtn = findViewById(R.id.logoutbtn);
+
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        CheckUser();
+
+
+//        logoutbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                CheckUser();
+//            }
+//        });
+
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.Frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void CheckUser(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user == null){
+            startActivity(new Intent(MainUserActivity.this,LoginActivity.class));
+            finish();
+        }else{
+            loadMyInfo();
+        }
+    }
+
+    private void loadMyInfo(){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                        for(DataSnapshot ds : datasnapshot.getChildren()){
+                            String name = ""+ds.child("name").getValue();
+
+                            nameTV.setText(name);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/flashcart/MainsellerActivity.java
+++ app/src/main/java/com/example/flashcart/MainsellerActivity.java
@@ -1,14 +1,83 @@
 package com.example.flashcart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainsellerActivity extends AppCompatActivity {
+
+
+    private TextView nameTV;
+    private ImageButton logoutbtn;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainseller);
+
+
+        nameTV = findViewById(R.id.nametv);
+        logoutbtn = findViewById(R.id.logoutbtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        CheckUser();
+
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckUser();
+            }
+        });
+
+
+
     }
+
+    private void CheckUser(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user == null){
+            startActivity(new Intent(MainsellerActivity.this,LoginActivity.class));
+            finish();
+        }else{
+            loadMyInfo();
+        }
+    }
+
+    private void loadMyInfo(){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                        for(DataSnapshot ds : datasnapshot.getChildren()){
+                            String name = ""+ds.child("name").getValue();
+
+                            nameTV.setText(name);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+
+
 }
