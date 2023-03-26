f0a4c47c2dd49e4977d0a77a8140b20dc35a54a8
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2023-03-26 14:37:18 +0530

    Worked on order page...........

---
+++ app/src/main/java/com/example/flashcart/Adaptor/AdaptorOrderDetailUser.java
@@ -0,0 +1,111 @@
+package com.example.flashcart.Adaptor;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flashcart.Model.ModelCartItemRecieve;
+import com.example.flashcart.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class AdaptorOrderDetailUser extends RecyclerView.Adapter<AdaptorOrderDetailUser.HolderCartItem> {
+
+    private Context context;
+    private ArrayList<ModelCartItemRecieve> cartItems;
+
+    public AdaptorOrderDetailUser(Context context, ArrayList<ModelCartItemRecieve> cartItems) {
+        this.context = context;
+        this.cartItems = cartItems;
+    }
+
+    @NonNull
+    @Override
+    public HolderCartItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.row_orderdetail_user,parent,false);
+
+        return new HolderCartItem(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderCartItem holder, int position) {
+
+        //get data
+
+        ModelCartItemRecieve modelCartItemRecieve = cartItems.get(position);
+
+        String pid = modelCartItemRecieve.getItemUid();
+        String title = modelCartItemRecieve.getTitle();
+        String price = modelCartItemRecieve.getPrice();
+        String quantity = modelCartItemRecieve.getQuantity();
+        String producticon = modelCartItemRecieve.getProductIcon();
+
+
+        //set data
+
+        holder.itemtitle1TV.setText(""+title);
+        holder.itemprice1TV.setText("₹"+price);
+        holder.quantity1tv.setText(""+quantity);
+
+
+        try{
+
+            Picasso.get().load(producticon).placeholder(R.drawable.baseline_add_shopping_black_cart_24).into(holder.productIconTv);
+
+
+        }catch (Exception e){
+
+            holder.productIconTv.setImageResource(R.drawable.baseline_add_shopping_black_cart_24);
+
+        }
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return cartItems.size();
+    }
+
+    //view holder class
+
+    class HolderCartItem extends RecyclerView.ViewHolder{
+
+
+
+        //init view
+         TextView itemtitle1TV,itemprice1TV,quantity1tv,itemdeletebtn,instock;
+        ImageView productIconTv;
+
+        public HolderCartItem(@NonNull View itemView) {
+            super(itemView);
+
+            itemtitle1TV = itemView.findViewById(R.id.itemtitleTv);
+            itemprice1TV = itemView.findViewById(R.id.itempriceTv);
+            quantity1tv = itemView.findViewById(R.id.itemQuantityTv);
+            productIconTv = itemView.findViewById(R.id.itemIconTv);
+
+
+
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/flashcart/UserPage/UserOrderDetailFragment.java
+++ app/src/main/java/com/example/flashcart/UserPage/UserOrderDetailFragment.java
@@ -1,20 +1,51 @@
 package com.example.flashcart.UserPage;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.flashcart.Adaptor.AdaptorOrderDetailUser;
+import com.example.flashcart.Model.ModelCartItemRecieve;
 import com.example.flashcart.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class UserOrderDetailFragment extends Fragment {
 
 
     String orderTo,orderId;
+
+    TextView orderdetailId,orderdetaildate,orderdetailstatus,orderdetailshopname,orderdetailitem,orderdetailfinalamount,orderdetailaddress;
+    RecyclerView recycler1;
+
+    TextView orderdate,orderdeliverTv;
+
+    ProgressBar progressBar;
+
+    FirebaseAuth firebaseAuth;
+
+    ArrayList<ModelCartItemRecieve> orderUserArrayList;
+    AdaptorOrderDetailUser adaptorCartItem;
+
 
 
     public UserOrderDetailFragment() {
@@ -30,6 +61,24 @@ public class UserOrderDetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_user_order_detail, container, false);
 
 
+        orderdetailId = view.findViewById(R.id.orderdetailId);
+        orderdetaildate = view.findViewById(R.id.orderdetaildate);
+        orderdetailstatus = view.findViewById(R.id.orderdetailstatus);
+        orderdetailshopname = view.findViewById(R.id.orderdetailshopname);
+        orderdetailitem = view.findViewById(R.id.orderdetailitem);
+        orderdetailfinalamount = view.findViewById(R.id.orderdetailfinalamount);
+        orderdetailaddress = view.findViewById(R.id.orderdetailaddress);
+        orderdate  = view.findViewById(R.id.text9);
+        progressBar = view.findViewById(R.id.progress);
+        orderdeliverTv = view.findViewById(R.id.orderdeliverTv);
+
+
+        recycler1 = view.findViewById(R.id.recycler1);
+        recycler1.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         Bundle bundle = getArguments();
         if (bundle != null) {
@@ -39,6 +88,144 @@ public class UserOrderDetailFragment extends Fragment {
         }
 
 
+        loadshopInfo();
+        loadorderDetails();
+        loadorderItems();
+
+
         return view;
+    }
+
+    private void loadorderItems() {
+
+        orderUserArrayList = new ArrayList<>();
+        adaptorCartItem = new AdaptorOrderDetailUser(getContext(),orderUserArrayList);
+        recycler1.setAdapter(adaptorCartItem);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+
+        ref.child(orderTo).child("Orders").child(orderId).child("Items")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+
+                        orderUserArrayList.clear();
+
+                        for (DataSnapshot ds : datasnapshot.getChildren()){
+
+                            ModelCartItemRecieve modelOrderUser = ds.getValue(ModelCartItemRecieve.class);
+
+                            orderUserArrayList.add(modelOrderUser);
+
+                        }
+
+                        //all item added to list now set adaptor
+
+                        adaptorCartItem = new AdaptorOrderDetailUser(getContext(),orderUserArrayList);
+
+                        recycler1.setAdapter(adaptorCartItem);
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+    }
+
+    private void loadorderDetails() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+
+        ref.child(orderTo).child("Orders").child(orderId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+
+                        String orderBy=""+datasnapshot.child("orderBy").getValue();
+
+                        String orderCost = ""+datasnapshot.child("orderFinalSubTotal").getValue();
+
+                        String orderId = ""+datasnapshot.child("orderId").getValue();
+
+                        String orderStatus = ""+datasnapshot.child("orderStatus").getValue();
+
+                        String orderTime=""+datasnapshot.child("orderTime").getValue();
+
+                        String orderTo=""+datasnapshot.child("orderTo").getValue();
+
+                        String deliveryFee=""+datasnapshot.child("deliveryFee").getValue();
+
+
+                        //convert timestamp to proper format
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTimeInMillis(Long.parseLong(orderTime));
+
+                        String formatdate = DateFormat.format("dd/MM/yyyy hh:mm a",calendar).toString();
+
+
+                        if (orderStatus.equals("In Progress")){
+                            orderdetailstatus.setTextColor(getResources().getColor(R.color.purple_200));
+                            progressBar.setProgress(50);
+
+
+                        } else if (orderStatus.equals("Complete")) {
+                            orderdetailstatus.setTextColor(getResources().getColor(R.color.colorGreen1));
+                            progressBar.setProgress(100);
+
+                        } else if (orderStatus.equals("Cancelled")) {
+
+                            orderdetailstatus.setTextColor(getResources().getColor(R.color.red));
+                            progressBar.setProgress(100);
+                            progressBar.setProgressTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
+                            orderdeliverTv.setText("Order Cancelled");
+                            orderdeliverTv.setTextColor(getResources().getColor(R.color.red));
+
+
+                        }
+
+
+                        orderdetailId.setText(orderId);
+                        orderdetailstatus.setText(orderStatus);
+                        orderdetailfinalamount.setText(orderCost);
+                        orderdetaildate.setText(formatdate);
+                        orderdate.setText(formatdate);
+
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+    }
+
+    private void loadshopInfo() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(orderTo)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                        String shopName = ""+datasnapshot.child("shopName").getValue();
+                        orderdetailshopname.setText(shopName);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
     }
 }
